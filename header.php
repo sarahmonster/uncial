5f3d6f86321b6fb3fcbeee3be01176738ddeba55
sarah ✈ semark <sarah@triggersandsparks.com>
2014-06-01 19:35:25 -0400

    Add logo and fix header to top of page.

--- header.php
+++ header.php
@@ -47,12 +47,7 @@
 
 				<div id="inner-header" class="wrap cf">
 
-					<?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
-					<p id="logo" class="h1"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
-
-					<?php // if you'd like to use the site description you can un-comment it below ?>
-					<?php // bloginfo('description'); ?>
-
+					<a id="logo" href="<?php echo home_url(); ?>" rel="nofollow"><img src="<?php echo get_template_directory_uri(); ?>/library/images/logo.png" alt="<?php echo bloginfo('name'); ?>"></a>
 
 					<nav role="navigation">
 						<?php wp_nav_menu(array(
