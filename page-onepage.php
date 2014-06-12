a343d2c97f82c3ee108b18ec2c252de7386b2ee9
sarah ✈ semark <sarah@triggersandsparks.com>
2014-06-11 22:51:50 -0400

    Remove titles from pages.

--- page-onepage.php
+++ page-onepage.php
@@ -15,13 +15,13 @@ foreach ($pages as $post) :
     <div id="<?php echo the_slug(); ?>" class="page-panel" data-slide="<?php echo $count; ?>" data-stellar-background-ratio="0.5">
     
         <div class="inner-content wrap clearfix">
-            <div class="twelvecol first last clearfix entry-content" role="main">
-                <?php if (the_slug() != "home"):  echo the_title('<h1>', '</h1>');  endif; ?>
+            <div class="clearfix entry-content" role="main">
+                <?php //if (the_slug() != "home"):  echo the_title('<h1>', '</h1>');  endif; ?>
                 <?php echo the_content(); ?>
                 
                 <?php if (the_slug() == "news"): ?>
                     <?php get_template_part('news'); ?>
-                    <div class="twelvecol first last clearfix center"><a class="button" href="/news">Read all news posts</a></div>
+                    <div class="clearfix center"><a class="button" href="/news">Read all news posts</a></div>
                 <?php endif; ?> 
             
             </div>
