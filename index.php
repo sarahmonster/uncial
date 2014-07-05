34dcc489ba1d7f39bad715a4595105c7f280df36
sarah ✈ semark <sarah@triggersandsparks.com>
2014-07-04 21:02:18 -0400

    Set up simple page for all news posts.

--- index.php
+++ index.php
@@ -12,29 +12,20 @@
 
 								<header class="article-header">
 
-									<h1 class="h2 entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
-									<p class="byline vcard">
-										<?php printf( __( 'Posted', 'bonestheme' ) . ' <time class="updated" datetime="%1$s" pubdate>%2$s</time> ' . __('by', 'bonestheme' ) . ' <span class="author">%3$s</span>', get_the_time('Y-m-j'), get_the_time(get_option('date_format')), get_the_author_link( get_the_author_meta( 'ID' ) )); ?>
-									</p>
+									<h2 class="byline vcard">
+					                    <time class="updated" datetime="<?php echo get_the_time('Y-m-j'); ?>" pubdate>
+					                    	<span><?php echo get_the_time('l,'); ?></span>
+					                    	<?php echo get_the_time('F jS, Y'); ?>
+					                    </time>
+					                  </h2>
+
+					                <h3 class="entry-title single-title" itemprop="headline"><?php the_title(); ?></h3>	
 
 								</header>
 
 								<section class="entry-content cf">
-									<?php the_content(); ?>
+									<?php the_excerpt(); ?>
 								</section>
-
-								<footer class="article-footer cf">
-									<p class="footer-comment-count">
-										<?php comments_number( __( '<span>No</span> Comments', 'bonestheme' ), __( '<span>One</span> Comment', 'bonestheme' ), _n( '<span>%</span> Comments', '<span>%</span> Comments', get_comments_number(), 'bonestheme' ) );?>
-									</p>
-
-
-                 	<?php printf( '<p class="footer-category">' . __('filed under', 'bonestheme' ) . ': %1$s</p>' , get_the_category_list(', ') ); ?>
-
-                  <?php the_tags( '<p class="footer-tags tags"><span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '</p>' ); ?>
-
-
-								</footer>
 
 							</article>
 
