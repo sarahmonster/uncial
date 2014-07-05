868e531628f6cef5ac8a2f181f43f7410636c197
sarah ✈ semark <sarah@triggersandsparks.com>
2014-07-04 20:55:01 -0400

    Show dates on news panel and redesign slightly.

--- news.php
+++ news.php
@@ -4,7 +4,11 @@ $posts = get_posts( $args );
 $news_count = 1;
 foreach ( $posts as $post ) : setup_postdata( $post ); ?>
 	<div class="news-excerpt">
-		<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+        <h2><time class="updated" datetime="<?php echo get_the_time('Y-m-j'); ?>" pubdate>
+            <span><?php echo get_the_time('l,'); ?></span>
+            <?php echo get_the_time('F jS, Y'); ?>
+        </time></h2>
+        <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
 		<?php the_excerpt(); ?>
 	</div>
 <?php 
